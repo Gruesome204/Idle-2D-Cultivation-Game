779d3c48f02b93ca0a916f59c55c17b81df02bea
Gruesome204 <lukas@logopaedie-schneider.de>
2024-12-17 15:48:02 +0100

    Updated

--- Assets/Scripts/Interaction/AncientRuinInteractable.cs
+++ Assets/Scripts/Interaction/AncientRuinInteractable.cs
@@ -1,22 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 public class AncientRuinInteractable : MonoBehaviour, IClickable
 {
 
     [SerializeField] GlobalGameDataSO globalGameDataSO;
 
+    public float realmStayDuration;
+    public string targetRealmSceneName;
+
     public void Interact()
     {
         Debug.Log($"Ancient Ruin Interaction");
+        //int num = Random.Range(0, globalGameDataSO.ancientRuinListe.Length);
+        //BaseAncientRuinData selectedRuin = globalGameDataSO.ancientRuinListe[num];
 
-      int num =   Random.Range( 2, globalGameDataSO.ancientRuinListe.Length);
-
-        SceneManager.LoadScene(num);
-        //globalGameDataSO.ancientRuinListe[num];
-       
+        //realmStayDuration = selectedRuin.realmStayDuration;
+        //targetRealmSceneName = selectedRuin.targetRealmSceneName;
+        //StartCoroutine(TeleportToRealm(realmStayDuration));
+        //SceneManager.LoadScene($"{selectedRuin.targetRealmSceneName}");
+        ////globalGameDataSO.ancientRuinListe[num];
 
     }
+
+    //private IEnumerator TeleportToRealm(float realmStayDuration)
+    //{
+    //    AsyncOperation loadOperation = SceneManager.LoadSceneAsync(targetRealmSceneName, LoadSceneMode.Additive);
+    //    while (!loadOperation.isDone)
+    //    {
+    //        yield return null;
+    //    }
+    //    yield return new WaitForSeconds(5);
+
+    //    AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(targetRealmSceneName);
+    //    while (!unloadOperation.isDone)
+    //    {
+    //        yield return null;
+    //    }
+    //}
 }
